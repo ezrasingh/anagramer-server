b7c40f7d92cbe8b923db38f4dbe33668e093d4df
Ezra Singh <ezra@lionsolutions.tech>
2018-07-15 23:01:04 -0400

    Added prime hash normalization test

--- src/anagramer/test_utils.py
+++ src/anagramer/test_utils.py
@@ -1,7 +1,11 @@
+from itertools import permutations
 from anagramer.utils import normalize, alphabetize
 
 def test_normalize():
-    assert normalize("hello") == "fk:ehllo"
+    # prime hash = 23(h) * 2(e) * 31(l) * 31(l) * 7(o)
+    prime_hash = bytes(309442)
+    for anagram in permutations("hello"):
+        assert normalize("hello") == normalize(anagram) == prime_hash
 
 def test_alphabetize():
     sorted_collection = alphabetize(["apple", "bear", "candy"])
