f913fcf240f2af30e415340021b2b185cce5d463
Ezra Singh <singhezra@gmail.com>
2018-07-15 23:53:21 -0400

    Reverted back to string sorting as normalization method

--- src/anagramer/test_utils.py
+++ src/anagramer/test_utils.py
@@ -2,10 +2,8 @@ from itertools import permutations
 from anagramer.utils import normalize, alphabetize
 
 def test_normalize():
-    # prime hash = 23(h) * 2(e) * 31(l) * 31(l) * 7(o)
-    prime_hash = bytes(309442)
     for anagram in permutations("hello"):
-        assert normalize("hello") == normalize(anagram) == prime_hash
+        assert normalize("hello") == normalize(anagram)
 
 def test_alphabetize():
     sorted_collection = alphabetize(["apple", "bear", "candy"])
