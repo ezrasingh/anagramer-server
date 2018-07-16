62f93a76bfdc334b505cd3a0ebdfae700174e1ed
Ezra Singh <ezra@lionsolutions.tech>
2018-07-15 22:40:19 -0400

    Added alphabetize unit test

--- src/anagramer/test_utils.py
+++ src/anagramer/test_utils.py
@@ -2,3 +2,7 @@ from anagramer.utils import normalize, alphabetize
 
 def test_normalize():
     assert normalize("hello") == "fk:ehllo"
+
+def test_alphabetize():
+    sorted_collection = alphabetize(["apple", "bear", "candy"])
+    assert set(sorted_collection) ^ set(["candy", "bear", "apple"]) == set()
